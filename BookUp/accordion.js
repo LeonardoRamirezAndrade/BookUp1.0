aae5903d71803b46d2f43bc5cf01dc49ad395b42
LeonardoRamirezAndrade <leonardoramireznegocios@gmail.com>
2022-03-11 22:49:04 -0500

    última versión

--- BookUp/accordion.js
+++
@@ -1,7 +0,0 @@
-const accordion = document.getElementsByClassName('contentBx');
-
-for (i = 0; i<accordion.length; i++){
-    accordion[i].addEventListener('click', function(){
-        this.classList.toggle('active')
-    })
-}
--- Carpeta BookUp/BookUp/README.md
+++
@@ -1,3 +0,0 @@
-# BookUp
-Nosotros somos BookUp y te invitamos a disfrutar de nuestra biblioteca escolar completamente gratuita.
-Ya logramos colocar todas las categorías dentro de poco podras difrutarla.
